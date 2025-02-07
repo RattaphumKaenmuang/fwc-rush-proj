8fcc4f4f722cb4dcc665bb1b5a63f14e9a474802
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2025-02-08 01:03:10 +0700

    feat: add jQuery for text randomization effect and update CV description

---
+++ js/rkaenmua.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function(){
+    function randomise_text(element, max_iterations){
+        var letters = "abcdefghijklmnopqrstuvwxyz#%&^+=-";
+        const real_text = element.text();
+        
+        let iterations = 0;
+        const interval = setInterval(() => {
+            let temp_text = "";
+            for (let i = 0; i < real_text.length; i++) {
+                let rand_idx = Math.floor(Math.random() * letters.length);
+                let random_char = letters[rand_idx];
+                temp_text += random_char;
+            }
+            element.text(temp_text);
+            iterations++;
+
+            if (iterations >= max_iterations) {
+                clearInterval(interval);
+                element.text(real_text);
+            }
+        }, 50);
+    }
+
+    randomise_text($("#name"), 15);
+    randomise_text($("#desc"), 15);
+    randomise_text($("#long-desc"), 5);
+});
